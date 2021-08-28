23317d9e6736e51ccf61316e5b163a5bff0d6c07
alsav22 <alsav22@rarmbler.ru>
2021-08-28 12:19:27 +0300

    3

--- Program.cs
+++ Program.cs
@@ -31,99 +31,19 @@ namespace DR
             //{
             //    Console.WriteLine(port);
             //}
+            
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            //RemoteControl.device = new Device();
-
-            //if (RemoteControl.device.ID_VID != "" && RemoteControl.device.ID_PID != "")
-            //{
-            //    if (RemoteControl.device.deviceSearch(RemoteControl.device.ID_VID, RemoteControl.device.ID_PID))
-            //    {
-            //        RemoteControl.device.connection();
-                    
-            //    }
-            //    else
-            //        MessageBox.Show("Device not found!");
-            //}
-
-
-            //RemoteControl.form = new Form1(RemoteControl.device.portName, RemoteControl.device.ID_VID, RemoteControl.device.ID_PID);
-            //RemoteControl.form.Show();
-            
-            ////if (RemoteControl.lilstPresets.Count == 0)
-            //    //form.buttonSettings.Hide();
-
-            //if (RemoteControl.device.portName != "" && RemoteControl.device.portName != null)
-            //{
-            //    MessageBox.Show("The device is connected and ready for use.");
-
-            //    RemoteControl.form.ActiveControl = null;
-            //    RemoteControl.form.WindowState = FormWindowState.Minimized;
-            //}
-
-            //if (RemoteControl.device.portName != "")
-            //{
-            //    RemoteControl RC = new RemoteControl();
-            //    RC.start();
-            //}
-
-            
-            //List<string> list = new List<string>();
-            //list.AddRange(Enum.GetNames(typeof(Keys)));
-            //string[] arrstr = Enum.GetNames(typeof(Keys));
-            //DataForHotkeys obj = new DataForHotkeys();
-            //string[] arrstr;
-            //arrstr = File.ReadAllLines("Данные для HotKey.txt");
-            //list.AddRange(arrstr);
-            //char [] trim = {'{', '}'};
-            //for (int i = 0; i < list.Count; ++i)
-            //{
-            //    string[] parts = list[i].Split(' ');
-            //    if (parts.Length > 1)
-            //    {
-            //        if (parts[0] == "Space")
-            //            parts[1] = " ";
-            //        parts[1] = parts[1].Trim(trim);
-            //        parts[1] = "{" + parts[1] + "}";
-            //        list[i] = parts[0] + " " + parts[1];
-                    
-
-            //    }
-            //}
-            //while ((str = sr.ReadLine()) != null)
-            //{
-
-            //    string[] parts = str.Split(' ');
-            //    if (!dictionaryHotKeys.ContainsKey(parts[0]))
-            //    {
-            //        if (parts.Length > 1)
-            //        {
-            //            if (parts[0] == "Space")
-            //                parts[1] = " ";
-
-            //            dictionaryHotKeys.Add(parts[0], parts[1]);
-
-            //        }
-            //        else
-            //        {
-
-            //            dictionaryHotKeys.Add(parts[0], "");
-
-            //        }
-            //    }
-            //}
-            
-            //File.WriteAllLines("list.txt", list);
-            //Preset p = new Preset();
-            //p.readHotKeys();
+           
 
             RemoteControl RC = new RemoteControl();
             RC.start();
             
             Application.Run();
             
-            //Environment.Exit(0);
+            
         }
 
     }
