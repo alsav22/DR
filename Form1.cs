5e729c73a98d22308c45749a833d4caafc9ad807
alsav22 <alsav22@rarmbler.ru>
2021-09-04 15:15:19 +0300

    Добавил поиск и вывод названия активного окна

--- Form1.cs
+++ Form1.cs
@@ -7,17 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
+//using System.Management;
+using System.Runtime.InteropServices;
 
 namespace DR
 {
+    delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+    
+    
     partial class Form1 : Form
     {
-
+        static TextBox _tb;
+        
         public string PresetNameText { get { return presetName.Text; } set { presetName.Text = value; } }
 
         public Form1(string portName, string ID_VID, string ID_PID)
         {
             InitializeComponent();
+
+            /////////////////////////////
+
+            WinEventDelegate dele = new WinEventDelegate(WinEventProc);
+            IntPtr m_hhook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, dele, 0, 0, WINEVENT_OUTOFCONTEXT);
+            _tb = this.processName;
+
+            /////////////////////////////
             
             this.portName.Text = portName;
             id_vid.Text = ID_VID;
@@ -165,6 +179,40 @@ namespace DR
             Application.Exit();
             
         }
+///////////////////////////////////
+        
+        private static void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            string wTitle = GetActiveWindowTitle();
+            _tb.Clear();
+            _tb.AppendText((!String.IsNullOrEmpty(wTitle) ? wTitle : "Empty") + "\r\n"); // вместо этого реализуйте метод записи в файл
+        }
+ 
+        private static string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            IntPtr handle = IntPtr.Zero;
+            StringBuilder Buff = new StringBuilder(nChars);
+            handle = GetForegroundWindow();
+ 
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+ 
+ 
+ //////////////////////////////////////
+ 
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+        private const uint WINEVENT_OUTOFCONTEXT = 0;
+        private const uint EVENT_SYSTEM_FOREGROUND = 3;
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
     }
-
+    
 }
